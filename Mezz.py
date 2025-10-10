d976634b536e45628432e1ec3b4149c48f93c042
Rimjhimrani <144531476+Rimjhimrani@users.noreply.github.com>
2025-10-10 11:06:22 +0530

    Update Mezz.py

--- Mezz.py
+++ Mezz.py
@@ -182,7 +182,7 @@ def create_single_sticker(row, part_no_col, desc_col, max_capacity_col, all_mode
     store_loc_table.setStyle(TableStyle([('GRID', (0, 0), (-1, -1), 1, colors.black), ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
                                           ('ALIGN', (0, 0), (-1, -1), 'CENTER')]))
     sticker_content.append(store_loc_table)
-    sticker_content.append(Spacer(1, 0.3*cm))
+    sticker_content.append(Spacer(1, 0.5*cm))
     
     # --- Bottom Row Logic (MTM and QR) ---
     bottom_row_width = PADDED_CONTENT_WIDTH
